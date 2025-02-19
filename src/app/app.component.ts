0713e43955ed2dee47c37a4632fe642b46f46978
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-19 14:34:09 -0300

    create component big-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent],
+  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
