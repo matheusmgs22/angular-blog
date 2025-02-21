aaa206743c2b88540ada73e6f167f78d0872e0a2
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-20 22:33:39 -0300

    Creating other pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, HomeComponent],
+  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, HomeComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
 
+    photoCover:string=""
+    contentTitle:string=""
+    contentDescription:string=""
+
+    ngOnInit(): void {
+
+    }
 }
