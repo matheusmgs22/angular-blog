9c5cc0e1e97ae0d341e8036dc5b204eccd1827f1
matheusmgs22 <matheusmgs17@gmail.com>
2025-03-22 09:23:31 -0300

    Finalizando content component

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,64 +1,82 @@
 export const dataFake = {
-    bigCards: [
-      {
-        Id: 1,
-        cardTitle: "A Revolução da Inteligência Artificial no Dia a Dia",
-        cardDescription: "A Inteligência Artificial já faz parte da nossa rotina, desde assistentes virtuais até sistemas de automação. Descubra como ela está moldando o futuro e tornando nossas vidas mais inteligentes e conectadas.",
-        photoCover: "https://www.conectaoeste.com.br/images/noticias/58087/c53f2ea4af9206d91987e0bffbb98887.webp",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 2,
-        cardTitle: "Computação Quântica: O Futuro da Tecnologia",
-        cardDescription: "A computação quântica promete revolucionar a ciência e a tecnologia, resolvendo problemas complexos que antes pareciam impossíveis. Entenda como essa inovação pode impactar diferentes setores da sociedade.",
-        photoCover: "https://t.ctcdn.com.br/ytnvXCSCAkN7dV-nMMcvBxqQ8vc=/0x111:1600x1011/1200x675/smart/i339615.jpeg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 3,
-        cardTitle: "Cidades Inteligentes: Como a Tecnologia Está Transformando o Urbanismo",
-        cardDescription: "O uso de sensores, IoT e inteligência artificial está criando cidades mais eficientes e sustentáveis. Conheça os projetos que já estão em desenvolvimento ao redor do mundo.",
-        photoCover: "https://www.betha.com.br/blog/wp-content/uploads/2021/10/Blog-2.jpg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      }
-    ],
+  bigCards: [
+    {
+      Id: 1,
+      cardTitle: "A Revolução da Inteligência Artificial no Dia a Dia",
+      cardDescription: "A Inteligência Artificial já faz parte da nossa rotina, desde assistentes virtuais até sistemas de automação. Descubra como ela está moldando o futuro e tornando nossas vidas mais inteligentes e conectadas.",
+      photoCover: "https://www.conectaoeste.com.br/images/noticias/58087/c53f2ea4af9206d91987e0bffbb98887.webp",
+      contentText: `A Inteligência Artificial (IA) está revolucionando a forma como vivemos, trabalhamos e interagimos com o mundo ao nosso redor. Hoje, a IA já é parte integrante da nossa rotina: desde assistentes virtuais que gerenciam nossas agendas e tocam músicas, até sistemas avançados de automação que transformam processos industriais e serviços ao consumidor. Este avanço não só tem acelerado a inovação tecnológica, mas também abriu portas para um futuro onde a IA poderá antecipar necessidades, resolver problemas complexos e personalizar experiências em tempo real.
 
-    smallCards: [
-      {
-        Id: 4,
-        cardTitle: "Carros Autônomos: O Futuro da Mobilidade Urbana",
-        photoCover: "https://i0.statig.com.br/bancodeimagens/6b/cd/aq/6bcdaqrt51hlc9cj3b8fb7sj6.jpg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 5,
-        cardTitle: "Metaverso: A Nova Era da Internet",
-        photoCover: "https://agenciagentileza.com.br/wp-content/uploads/2022/08/Metaverso-O-que-e-e-como-ele-promete-mudar-o-futuro-1.jpg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 6,
-        cardTitle: "Google lança ferramenta de busca com inteligência artificial",
-        photoCover: "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/507004930-google-ia-768x512.jpg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 7,
-        cardTitle: "Energia Sustentável: O Papel da Tecnologia na Preservação do Planeta",
-        photoCover: "https://scansource.com.br/wp-content/uploads/2024/01/1-scaled.jpg",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 8,
-        cardTitle: "Apple aposta em realidade aumentada com novo dispositivo",
-        photoCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMrWi2nRD0EuYFv9yr2ILckiw7pWeyxthPcQ&s",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      },
-      {
-        Id: 9,
-        cardTitle: "Cybersegurança em 2025: Como proteger seus dados?",
-        photoCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBnQ_hVHxoNdHfIAzKWipvsrXS17oC6R_5OQ&s",
-        contentText: "Texto completo da notícia sobre Inteligência Artificial..."
-      }
-    ]
-  };
+Empresas de diversos setores estão investindo pesado em algoritmos inteligentes para melhorar a eficiência operacional, reduzir custos e oferecer um atendimento mais personalizado. Além disso, o impacto social é imenso, com a IA contribuindo para diagnósticos médicos mais precisos, sistemas de transporte mais seguros e até na criação de conteúdos e artes. O caminho não é isento de desafios éticos e de privacidade, mas a promessa de uma sociedade mais conectada e inteligente mantém o investimento e a pesquisa nesse campo em alta. Este é o momento de explorar como a Inteligência Artificial está, silenciosamente, reescrevendo as regras do jogo em todas as esferas da vida.`
+    },
+    {
+      Id: 2,
+      cardTitle: "Computação Quântica: O Futuro da Tecnologia",
+      cardDescription: "A computação quântica promete revolucionar a ciência e a tecnologia, resolvendo problemas complexos que antes pareciam impossíveis. Entenda como essa inovação pode impactar diferentes setores da sociedade.",
+      photoCover: "https://t.ctcdn.com.br/ytnvXCSCAkN7dV-nMMcvBxqQ8vc=/0x111:1600x1011/1200x675/smart/i339615.jpeg",
+      contentText: `A computação quântica é considerada a próxima grande revolução tecnológica, com o potencial de resolver problemas que hoje desafiam até os computadores mais poderosos. Utilizando os princípios da mecânica quântica, como superposição e entrelaçamento, esses computadores operam de maneira totalmente diferente dos sistemas tradicionais. Essa tecnologia promete acelerar cálculos extremamente complexos, com aplicações que vão desde a descoberta de novos medicamentos até a resolução de enigmas matemáticos que podem transformar a criptografia e a segurança digital.
+
+Embora a tecnologia ainda esteja em estágios iniciais, gigantes do setor de tecnologia e startups de ponta estão investindo fortemente para superar os desafios técnicos e criar protótipos funcionais. O futuro da computação quântica não só promete acelerar inovações em setores industriais e científicos, mas também provocar uma transformação radical na forma como processamos e entendemos informações. Este campo emergente nos convida a repensar os limites da tecnologia e vislumbrar um mundo onde o impossível se torna alcançável.`
+    },
+    {
+      Id: 3,
+      cardTitle: "Cidades Inteligentes: Como a Tecnologia Está Transformando o Urbanismo",
+      cardDescription: "O uso de sensores, IoT e inteligência artificial está criando cidades mais eficientes e sustentáveis. Conheça os projetos que já estão em desenvolvimento ao redor do mundo.",
+      photoCover: "https://www.betha.com.br/blog/wp-content/uploads/2021/10/Blog-2.jpg",
+      contentText: `As cidades do futuro já estão se materializando hoje, graças à convergência de tecnologias como sensores, Internet das Coisas (IoT) e inteligência artificial. Esse novo paradigma, conhecido como “cidades inteligentes”, visa otimizar o uso de recursos, reduzir o desperdício e melhorar a qualidade de vida dos cidadãos. Projetos pioneiros ao redor do mundo estão implementando sistemas que monitoram tráfego, qualidade do ar, consumo de energia e até a segurança pública, permitindo uma gestão urbana mais eficiente e reativa.
+
+Essa transformação digital no urbanismo proporciona não apenas uma infraestrutura mais moderna, mas também a criação de espaços urbanos mais sustentáveis e resilientes. A integração de tecnologias permite que as cidades respondam em tempo real a desafios ambientais e sociais, promovendo uma mobilidade mais inteligente, gestão energética otimizada e serviços públicos de qualidade. Ao mesmo tempo, os cidadãos se beneficiam de uma maior transparência na administração e de uma experiência urbana personalizada, onde dados e tecnologia se unem para criar ambientes mais humanos e conectados.`
+    }
+  ],
+
+  smallCards: [
+    {
+      Id: 4,
+      cardTitle: "Carros Autônomos: O Futuro da Mobilidade Urbana",
+      photoCover: "https://i0.statig.com.br/bancodeimagens/6b/cd/aq/6bcdaqrt51hlc9cj3b8fb7sj6.jpg",
+      contentText: `Os carros autônomos estão redefinindo o conceito de mobilidade urbana, prometendo uma revolução no trânsito e na forma como nos deslocamos. Com sensores avançados, câmeras e algoritmos de inteligência artificial, esses veículos têm a capacidade de analisar o ambiente e tomar decisões em tempo real, aumentando a segurança e a eficiência nas ruas. Imagine um futuro onde engarrafamentos são coisa do passado, e o transporte público e privado se integram perfeitamente para oferecer viagens mais rápidas e confortáveis.
+
+Além de melhorar a segurança ao reduzir os erros humanos, os carros autônomos podem transformar a economia urbana, liberando tempo dos passageiros e otimizando rotas de entrega e transporte de mercadorias. Esse avanço tecnológico também promete reduzir a emissão de poluentes, contribuindo para um ambiente urbano mais sustentável. Apesar dos desafios regulatórios e técnicos que ainda precisam ser superados, a mobilidade autônoma representa um grande passo rumo a cidades mais inteligentes e conectadas.`
+    },
+    {
+      Id: 5,
+      cardTitle: "Metaverso: A Nova Era da Internet",
+      photoCover: "https://agenciagentileza.com.br/wp-content/uploads/2022/08/Metaverso-O-que-e-e-como-ele-promete-mudar-o-futuro-1.jpg",
+      contentText: `O metaverso surge como a próxima fronteira da internet, onde realidade virtual, aumentada e interações sociais convergem para criar ambientes imersivos e interativos. Essa nova era digital promete transformar a maneira como trabalhamos, jogamos e nos conectamos, criando experiências que ultrapassam as barreiras do mundo físico. Imagine participar de reuniões virtuais em espaços tridimensionais, explorar lojas e galerias de arte virtuais, ou simplesmente socializar com amigos em ambientes criados digitalmente.
+
+Grandes empresas de tecnologia estão investindo pesado para tornar essa visão uma realidade, desenvolvendo plataformas e dispositivos que possibilitam essa imersão total. O metaverso não se limita apenas a entretenimento; ele tem o potencial de revolucionar setores como educação, comércio e até mesmo a forma como realizamos transações financeiras. Apesar dos desafios e incertezas, essa nova era promete redefinir a experiência digital e criar um universo virtual paralelo, onde as possibilidades são ilimitadas.`
+    },
+    {
+      Id: 6,
+      cardTitle: "Google lança ferramenta de busca com inteligência artificial",
+      photoCover: "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/507004930-google-ia-768x512.jpg",
+      contentText: `A inovação no setor de buscas ganha novo impulso com o lançamento da ferramenta de busca do Google baseada em inteligência artificial. Essa nova abordagem utiliza algoritmos avançados para compreender melhor as intenções dos usuários, fornecendo resultados mais precisos e personalizados. Ao integrar IA, o Google não só otimiza a experiência de pesquisa, mas também revoluciona a forma como as informações são organizadas e apresentadas.
+
+Essa ferramenta é capaz de interpretar contextos complexos e aprender com as interações dos usuários, ajustando os resultados em tempo real. Para o consumidor, isso significa respostas mais relevantes e uma navegação mais intuitiva. Para o mercado, representa um avanço significativo que pode influenciar diversos setores, desde o comércio digital até o jornalismo, ao possibilitar uma análise mais profunda e detalhada dos dados coletados.`
+    },
+    {
+      Id: 7,
+      cardTitle: "Energia Sustentável: O Papel da Tecnologia na Preservação do Planeta",
+      photoCover: "https://scansource.com.br/wp-content/uploads/2024/01/1-scaled.jpg",
+      contentText: `Com a crescente preocupação ambiental e a necessidade urgente de reduzir a pegada de carbono, a tecnologia tem desempenhado um papel fundamental na transição para fontes de energia sustentáveis. Inovações em energias renováveis, como solar, eólica e hidroelétrica, estão sendo aprimoradas com o uso de sistemas inteligentes que otimizam a captação e distribuição de energia, tornando o consumo mais eficiente e menos impactante ao meio ambiente.
+
+Esses avanços tecnológicos não apenas promovem uma matriz energética mais limpa, mas também impulsionam a economia sustentável, gerando empregos e fomentando novas indústrias verdes. A integração de sistemas de monitoramento, IoT e inteligência artificial permite uma gestão energética em tempo real, contribuindo para a redução de desperdícios e para a maximização dos recursos disponíveis. Essa revolução na forma de produzir e consumir energia é essencial para a preservação do planeta e para um futuro mais equilibrado e resiliente.`
+    },
+    {
+      Id: 8,
+      cardTitle: "Apple aposta em realidade aumentada com novo dispositivo",
+      photoCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMrWi2nRD0EuYFv9yr2ILckiw7pWeyxthPcQ&s",
+      contentText: `A Apple, conhecida por suas inovações e design sofisticado, está expandindo seu portfólio com um novo dispositivo focado em realidade aumentada (AR). Este avanço promete transformar a experiência digital, permitindo que os usuários interajam com conteúdos virtuais de forma intuitiva e imersiva. Com um hardware poderoso e uma integração perfeita com seus ecossistemas de software, o novo dispositivo AR da Apple visa criar uma ponte entre o mundo físico e o digital, enriquecendo a forma como consumimos informação e entretenimento.
+
+Além do potencial para revolucionar setores como educação, saúde e entretenimento, essa aposta em AR reflete a visão futurista da Apple, que busca constantemente inovar e oferecer experiências únicas. O dispositivo combina design elegante, tecnologia de ponta e uma interface amigável, proporcionando uma experiência que promete ser não só funcional, mas também incrivelmente prazerosa para os usuários.`
+    },
+    {
+      Id: 9,
+      cardTitle: "Cybersegurança em 2025: Como proteger seus dados?",
+      photoCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBnQ_hVHxoNdHfIAzKWipvsrXS17oC6R_5OQ&s",
+      contentText: `Com o avanço da tecnologia e a crescente digitalização de informações, a cybersegurança se torna cada vez mais crítica. Em 2025, proteger dados pessoais e corporativos será um dos maiores desafios, dada a sofisticação dos ataques cibernéticos e a complexidade das infraestruturas digitais. Empresas e governos estão investindo massivamente em novas soluções de segurança, que incluem inteligência artificial, blockchain e análise comportamental para detectar e neutralizar ameaças em tempo real.
+
+Neste cenário, a educação e a conscientização sobre práticas de segurança cibernética se tornam essenciais para todos os usuários. Desde a implementação de protocolos avançados de criptografia até o treinamento constante de equipes especializadas, as estratégias de proteção evoluirão para acompanhar o ritmo acelerado das inovações tecnológicas. Proteger os dados em um mundo cada vez mais interconectado não é apenas uma necessidade técnica, mas uma questão de privacidade, confiança e segurança global.`
+    }
+  ]
+};
